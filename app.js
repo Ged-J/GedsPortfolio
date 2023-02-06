ca4c2d522d636efca9660977071464e5bb306e3b
gedvydas <74902029+Ged-J@users.noreply.github.com>
2023-02-06 02:06:09 +0000

    Added button for light and dark mode and etc
    Added feature for changing to light or dark mode , adjusted a bit of the text on the page

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ function PageTransitions() {
         }
     })
 
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
 
 }
 
